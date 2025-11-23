955a9ae718488fba85b3e5e9f9c913a4b934786a
arunrathi10 <200587971@student.georgianc.on.ca>
2025-11-23 00:43:27 -0500

    imported OpenAi and initialized using API from .env

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+const OpenAI = require('openai');
+const client = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Ask AI' });
 });
 
 module.exports = router;
